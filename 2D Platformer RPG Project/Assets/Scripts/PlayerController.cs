5ef07d8e0601d9556476312c963fcc5209c92def
Michael Chen <127451110+SerpentSea@users.noreply.github.com>
2023-07-25 01:27:16 +0300

    Player sprite flips when moving left

--- 2D Platformer RPG Project/Assets/Scripts/PlayerController.cs
+++ 2D Platformer RPG Project/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -67,7 +68,7 @@ public class PlayerController : MonoBehaviour
         }
 
     }
-    
+
     // Awake happens before Start()
     private void Awake()
     {
@@ -92,11 +93,45 @@ public class PlayerController : MonoBehaviour
         rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.velocity.y);
     }
 
+    public bool _isFacingRight = true;
+
+    public bool IsFacingRight { 
+        get 
+        { 
+            return _isFacingRight;  
+        }
+        private set
+        {
+            // Flip, if the value is new
+            if(_isFacingRight != value)
+            {
+                // Flip the local scale to make the player face the opposite direction
+                transform.localScale *= new Vector2(-1, 1);
+            }
+            _isFacingRight = value;
+        } }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if(moveInput.x > 0 && !IsFacingRight)
+        {
+            // Face the right
+            IsFacingRight = true;
+        }
+        else if(moveInput.x < 0 && IsFacingRight)
+        {
+            // Face the left
+            IsFacingRight = false;
+        }
+    }
+
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
 
         IsMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
     }
 
     public void OnRun(InputAction.CallbackContext context)
