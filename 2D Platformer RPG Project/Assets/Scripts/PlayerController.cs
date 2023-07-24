0e78d86f732f673f4444130774d6c5a8243047bb
Michael Chen <127451110+SerpentSea@users.noreply.github.com>
2023-07-24 22:17:07 +0300

    Player made, can move left and right

---
+++ 2D Platformer RPG Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    
+    public float walkSpeed = 5f;
+
+    Vector2 moveInput;
+
+    public bool isMoving { get; private set; }
+
+    private void Awake() // Awake happens before Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        isMoving = moveInput != Vector2.zero;
+    }
+}
