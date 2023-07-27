934de567740a62f75bcc707fee7caf681a35d1d1
Michael Chen <127451110+SerpentSea@users.noreply.github.com>
2023-07-27 18:39:37 +0300

    Player can jump and with correct animations

--- 2D Platformer RPG Project/Assets/Scripts/AnimationStrings.cs
+++ 2D Platformer RPG Project/Assets/Scripts/AnimationStrings.cs
@@ -9,5 +9,7 @@ internal class AnimationStrings
     internal static string isMoving = "isMoving";
     internal static string isRunning = "isRunning";
     internal static string isGrounded = "isGrounded";
+    internal static string yVelocity = "yVelocity";
+    internal static string jump = "jump";
 }
 
--- 2D Platformer RPG Project/Assets/Scripts/PlayerController.cs
+++ 2D Platformer RPG Project/Assets/Scripts/PlayerController.cs
@@ -4,14 +4,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     Animator animator;
-    
+    TouchingDirections touchingDirections;
+
     public float walkSpeed = 5f;
     public float runSpeed = 10f;
+    public float jumpImpulse = 8f;
 
     Vector2 moveInput;
 
@@ -74,6 +76,7 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        touchingDirections = GetComponent<TouchingDirections>();
     }
 
     // Start is called before the first frame update
@@ -91,6 +94,7 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.velocity.y);
+        animator.SetFloat(AnimationStrings.yVelocity, rb.velocity.y);
     }
 
     public bool _isFacingRight = true;
@@ -143,6 +147,16 @@ public class PlayerController : MonoBehaviour
         else if(context.canceled)
         {
             IsRunning = false;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        // To Do: Check if alive as well
+        if(context.started && touchingDirections.IsGrounded)
+        {
+            animator.SetTrigger(AnimationStrings.jump);
+            rb.velocity = new Vector2(rb.velocity.x, jumpImpulse);
         }
     }
 }
