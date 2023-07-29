0f3039fd9d6f8a1423e41a00e7db7d86ddc556ea
Michael Chen <127451110+SerpentSea@users.noreply.github.com>
2023-07-29 14:06:08 +0300

    First enemy created
    can currently move towards the right and left.

---
+++ 2D Platformer RPG Project/Assets/Scripts/Knight.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
+public class Knight : MonoBehaviour
+{
+    Rigidbody2D rb;
+    TouchingDirections touchingDirections;
+
+    public float walkSpeed = 3f;
+
+    public enum WalkableDirection
+    {
+        Right, Left
+    }
+
+    private WalkableDirection _walkDirection;
+    private Vector2 walkDirectionVector;
+
+    public WalkableDirection WalkDirection
+    {
+        get
+        {
+            return _walkDirection;
+        }
+        set
+        {
+            if(_walkDirection != value)
+            {
+                // Direction flipped
+                gameObject.transform.localScale = new Vector2(gameObject.transform.localScale.x * -1, gameObject.transform.localScale.y);
+
+                if(value == WalkableDirection.Right)
+                {
+                    walkDirectionVector = Vector2.right;
+                }
+                else if(value == WalkableDirection.Left)
+                {
+                    walkDirectionVector = Vector2.left;
+                }
+            }
+            _walkDirection = value;
+        }
+    }
+
+    private void FlipDirection()
+    {
+        if (WalkDirection == WalkableDirection.Right)
+        {
+            WalkDirection = WalkableDirection.Left;
+        }
+        else if (WalkDirection == WalkableDirection.Left)
+        {
+            WalkDirection = WalkableDirection.Right;
+        }
+        else
+        {
+            Debug.LogError("Walkable direction not set to legal values of right or left.");
+        }
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        touchingDirections = GetComponent<TouchingDirections>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(touchingDirections.IsGrounded && touchingDirections.IsOnWall)
+        {
+            FlipDirection();
+        }
+        rb.velocity = new Vector2(walkSpeed * walkDirectionVector.x, rb.velocity.y);
+    }
+}
