454681b4e9637fb11cfef9f05d84687b38ccd7a8
Michael Chen <127451110+SerpentSea@users.noreply.github.com>
2023-08-19 16:35:07 +0300

    Organized the SetBoolBehaviour

--- 2D Platformer RPG Project/Assets/Scripts/StateMachine/SetBoolBehaviour.cs
+++ 2D Platformer RPG Project/Assets/Scripts/StateMachine/SetBoolBehaviour.cs
@@ -18,12 +18,6 @@ public class SetBoolBehaviour : StateMachineBehaviour
         }
     }
 
-    // OnStateUpdate is called before OnStateUpdate is called on any state inside this state machine
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
     // OnStateExit is called before OnStateExit is called on any state inside this state machine
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -32,18 +26,6 @@ public class SetBoolBehaviour : StateMachineBehaviour
             animator.SetBool(boolName, valueOnExit);
         }
     }
-
-    // OnStateMove is called before OnStateMove is called on any state inside this state machine
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateIK is called before OnStateIK is called on any state inside this state machine
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
 
     // OnStateMachineEnter is called when entering a state machine via its Entry Node
     override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
