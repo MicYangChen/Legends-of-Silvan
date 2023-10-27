db0d1d9b1962b2638aa47a85645a594205d0bd12
Michael Chen <127451110+SerpentSea@users.noreply.github.com>
2023-10-28 00:44:57 +0300

    Player can Exit from the game

--- 2D Platformer RPG Project/Assets/Scripts/UIManager.cs
+++ 2D Platformer RPG Project/Assets/Scripts/UIManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UIManager : MonoBehaviour
 {
@@ -41,6 +42,23 @@ public class UIManager : MonoBehaviour
     {
         CharacterEvents.characterDamaged -= CharacterTookDamage;
         CharacterEvents.characterHealed -= CharacterHealed;
+    }
+
+    public void OnExitGame(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            #if (UNITY_EDITOR || DEVELOPMENT_BUILD)
+                Debug.Log(this.name + " : " + this.GetType() + " : " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+            #endif
+            #if (UNITY_EDITOR)
+                UnityEditor.EditorApplication.isPlaying = false;
+            #elif (UNITY_STANDALONE)
+                Application.Quit();
+            #elif (UNITY_WEBGL)
+                SceneManager.LoadScene("QuitScene");
+            #endif
+        }
     }
 
     private void Awake()
