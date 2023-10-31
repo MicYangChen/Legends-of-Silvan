e03d028fe267f21ecbf7c0505128cfc5d0502723
Michael Chen <127451110+SerpentSea@users.noreply.github.com>
2023-10-31 16:41:15 +0200

    Added Sound to HealthPickUp Script

--- 2D Platformer RPG Project/Assets/Scripts/HealthPickUp.cs
+++ 2D Platformer RPG Project/Assets/Scripts/HealthPickUp.cs
@@ -1,16 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class HealthPickUp : MonoBehaviour
 {
     public int healthRestore = 10;
     public Vector3 spinRotationSpeed = new Vector3(0, 180, 0);
 
-    void Start()
-    {
-        
-    }
+    public AudioClip pickUpSound;
+    public float volume = 1f;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -19,6 +19,7 @@ public class HealthPickUp : MonoBehaviour
         if(damageable)
         {
             damageable.Heal(healthRestore);
+            AudioSource.PlayClipAtPoint(pickUpSound, transform.position, volume);
             Destroy(gameObject);
         }
     }
@@ -27,5 +28,4 @@ public class HealthPickUp : MonoBehaviour
     {
         transform.eulerAngles += spinRotationSpeed * Time.deltaTime;
     }
-
 }
