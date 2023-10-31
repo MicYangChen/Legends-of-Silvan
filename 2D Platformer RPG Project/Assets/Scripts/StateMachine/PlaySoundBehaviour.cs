13a3aa4943a5324555b367e25814e7ca5add8b8d
Michael Chen <127451110+SerpentSea@users.noreply.github.com>
2023-10-31 16:16:55 +0200

    PlaySoundBehaviour Sounds from animator

---
+++ 2D Platformer RPG Project/Assets/Scripts/StateMachine/PlaySoundBehaviour.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundBehaviour : StateMachineBehaviour
+{
+    public AudioClip soundToPlay;
+    public float volume = 1f;
+    public bool playOnEnter = true, playOnExit = false, playAfterDelay = false;
+
+    public float playDelay = 0.25f;
+    private float timeSinceEntered = 0f;
+    private bool hasDelayedsoundPlayed = false;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (playOnEnter)
+        {
+            AudioSource.PlayClipAtPoint(soundToPlay, animator.gameObject.transform.position, volume);
+        }
+        timeSinceEntered = 0f;
+        hasDelayedsoundPlayed = false;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (playAfterDelay && !hasDelayedsoundPlayed)
+        {
+            timeSinceEntered += Time.deltaTime;
+
+            if (timeSinceEntered > playDelay)
+            {
+                AudioSource.PlayClipAtPoint(soundToPlay, animator.gameObject.transform.position, volume);
+                hasDelayedsoundPlayed = true;
+            }
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (playOnExit)
+        {
+            AudioSource.PlayClipAtPoint(soundToPlay, animator.gameObject.transform.position, volume);
+        }
+    }
+}
