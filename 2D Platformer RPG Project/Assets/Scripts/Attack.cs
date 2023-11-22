ecfb1cdbcd900ef487e632fb3631011e50770d70
Michael Chen <127451110+SerpentSea@users.noreply.github.com>
2023-11-22 21:35:55 +0200

    All melee attacks now gets buffed by item

--- 2D Platformer RPG Project/Assets/Scripts/Attack.cs
+++ 2D Platformer RPG Project/Assets/Scripts/Attack.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
     public int attackDamage = 10;
+    public int modifiedATT = 0;
     public Vector2 knockback = Vector2.zero;
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -17,7 +18,7 @@ public class Attack : MonoBehaviour
             Vector2 deliveredKnockback = transform.parent.localScale.x > 0 ? knockback : new Vector2(-knockback.x, knockback.y);
 
             // Hit the target
-            bool gotHit = damageable.Hit(attackDamage, deliveredKnockback);
+            bool gotHit = damageable.Hit(Mathf.Max(attackDamage, modifiedATT), deliveredKnockback);
             if(gotHit)
             {
                 Debug.Log(collision.name + " hit for " + attackDamage);
@@ -27,6 +28,6 @@ public class Attack : MonoBehaviour
 
     public void ChangeAttackDamage(int amount)
     {
-        attackDamage += amount;
+        modifiedATT = attackDamage += amount;
     }
 }
