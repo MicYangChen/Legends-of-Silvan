2c24ca805b69a7e2848671a7ac40efb51c11a5a2
Michael Chen <127451110+SerpentSea@users.noreply.github.com>
2023-11-27 16:48:57 +0200

    EnemyAttack Script + Player Attack depends on STR

--- 2D Platformer RPG Project/Assets/Scripts/Enemies/EnemyAttack.cs
+++ 2D Platformer RPG Project/Assets/Scripts/Enemies/EnemyAttack.cs
@@ -2,20 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attack : MonoBehaviour
+public class EnemyAttack : MonoBehaviour
 {
-    private PlayerStats playerStats;
-
-    private void Start()
-    {
-        playerStats = GameObject.Find("StatManager").GetComponent<PlayerStats>();
-    }
-
-    // public int attackDamage = 10;
-    private int modifiedATT = 0;
+    public int attackDamage = 10;
     private float randomMultiplier = 1f;
     public Vector2 knockback = Vector2.zero;
-
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -27,19 +18,14 @@ public class Attack : MonoBehaviour
             Vector2 deliveredKnockback = transform.parent.localScale.x > 0 ? knockback : new Vector2(-knockback.x, knockback.y);
 
             randomMultiplier = Random.Range(0.8f, 1.2f);
-            int damageDealt = Mathf.RoundToInt((Mathf.Max(playerStats.strength, modifiedATT) * randomMultiplier));
+            int damageDealt = Mathf.RoundToInt(attackDamage * randomMultiplier);
 
             // Hit the target
             bool gotHit = damageable.Hit(damageDealt, deliveredKnockback);
-            if(gotHit)
+            if (gotHit)
             {
                 Debug.Log(collision.name + " hit for " + damageDealt);
             }
         }
-    }
-
-    public void ChangeAttackDamage(int amount)
-    {
-        modifiedATT = playerStats.strength += amount;
     }
 }
