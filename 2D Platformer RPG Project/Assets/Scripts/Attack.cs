e1f678c2117d8e82f8c67f832e6addfbcdb12dd4
Michael Chen <127451110+SerpentSea@users.noreply.github.com>
2023-11-27 16:32:29 +0200

    Added AttackDamage randomness

--- 2D Platformer RPG Project/Assets/Scripts/Attack.cs
+++ 2D Platformer RPG Project/Assets/Scripts/Attack.cs
@@ -6,7 +6,10 @@ public class Attack : MonoBehaviour
 {
     public int attackDamage = 10;
     public int modifiedATT = 0;
+    public int damageDealt;
+    public float randomMultiplier = 1f;
     public Vector2 knockback = Vector2.zero;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -17,8 +20,11 @@ public class Attack : MonoBehaviour
         {
             Vector2 deliveredKnockback = transform.parent.localScale.x > 0 ? knockback : new Vector2(-knockback.x, knockback.y);
 
+            randomMultiplier = Random.Range(0.8f, 1.2f);
+            int damageDealt = Mathf.RoundToInt((Mathf.Max(attackDamage, modifiedATT) * randomMultiplier));
+
             // Hit the target
-            bool gotHit = damageable.Hit(Mathf.Max(attackDamage, modifiedATT), deliveredKnockback);
+            bool gotHit = damageable.Hit(damageDealt, deliveredKnockback);
             if(gotHit)
             {
                 Debug.Log(collision.name + " hit for " + attackDamage);
