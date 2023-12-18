205720bb1b21dadfe90a5cb436eeb3de17c59518
Michael Chen <127451110+SerpentSea@users.noreply.github.com>
2023-12-18 19:45:51 +0200

    Bow Fires Correct Direction

--- 2D Platformer RPG Project/Assets/Scripts/Player/ProjectileFire.cs
+++ 2D Platformer RPG Project/Assets/Scripts/Player/ProjectileFire.cs
@@ -29,9 +29,13 @@ public class ProjectileFire : MonoBehaviour
     public void FireProjectile()
     {
         GameObject projectile = Instantiate(projectilePrefab, launchPoint.position, projectilePrefab.transform.rotation);
-        Vector3 originalScale = projectile.transform.localScale;
 
-        projectile.transform.localScale = new Vector3(originalScale.x * transform.localScale.x > 0 ? 1 : -1,
-            originalScale.y, originalScale.z);
+        float angle = transform.eulerAngles.y;
+        Vector3 firingDirection = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad), 0);
+        
+        // Vector3 originalScale = projectile.transform.localScale;
+
+        projectile.transform.localScale = new Vector3(firingDirection.x * transform.localScale.x > 0 ? 1 : -1,
+            projectile.transform.localScale.y, projectile.transform.localScale.z);
     }
 }
